74c0f9debaca68aeb81f20a0df9118cce7572925
UnixUser <wevertonslima@gmail.com>
2022-12-06 15:44:47 -0300

    send move

--- server/server.js
+++ server/server.js
@@ -13,6 +13,19 @@ server.listen(8000);
 console.log('Server is running on port:8000');
 
 io.on('connection', (socket) => {
-    socket.on('move', (data) => {
+    console.log('user ' + socket.id + ' connected');
+    socket.on('join-room', (room, id) => {
+        socket.broadcast.emit('user-connected', id)
+        console.log(socket.id + ' received ' + id + ' peer, joining room: ' + room);
+        socket.on('make-move', (data) => {
+            if (isValidMove(move)) {
+                // envia a jogada para todos os outros usuários
+                socket.broadcast.emit('move-made', move);
+              }
+        });
+        socket.on('disconnect', () => {
+            socket.broadcast.emit('user-disconnected', id)
+            console.log('user ' + socket.id + ' disconnected');
+        });
     });
 });
