3e9d901aeda9d090f47a020d4b92385a85433a3a
UnixUser <wevertonslima@gmail.com>
2022-12-09 00:32:23 -0300

    now using chessboard-vue3 instead of vue3-package

--- server/server.js
+++ server/server.js
@@ -9,27 +9,48 @@ const io = require('socket.io')(server, {
     }
 });
 
+var players;
+var games = Array(100);
+for (let i = 0; i < 100; i++) {
+    games[i] = {players: 0 , pid: [0 , 0]};
+}
+
 server.listen(8888);
 console.log('Server is running on port:8888');
 
 io.on('connection', (socket) => {
-    console.log('user ' + socket.id + ' connected');
-    socket.on('join-room', (room, id) => {
-        socket.broadcast.emit('user-connected', id)
-        socket.broadcast.emit('move-made', 'a2-a4');
-        console.log(socket.id + ' received ' + id + ' peer, joining room: ' + room);
-        
-        socket.on('disconnect', () => {
-            socket.broadcast.emit('user-disconnected', id)
-            console.log('user ' + socket.id + ' disconnected');
-        });
+    socket.on('joined', function (roomId) {
+        if (games[roomId].players < 2) {
+            games[roomId].players++;
+            games[roomId].pid[games[roomId].players - 1] = playerId;
+        }
+        else {
+            socket.emit('full', roomId)
+            return;
+        }
+        console.log(games[roomId]);
+        players = games[roomId].players
+        if (players % 2 == 0) color = 'black';
+        else color = 'white';
+        socket.emit('player', {
+            playerId,
+            players,
+            color,
+            roomId
+        })
     });
-    socket.on('make-move', (data) => {
-        console.log('user ' + socket.id + ' está se movendo');
-        if (isValidMove(move)) {
-            // envia a jogada para todos os outros usuários
-            socket.broadcast.emit('move-made', move);
-            console.log('user ' + socket.id + ' moved ' + move);
-          }
+    socket.on('move', function (msg) {
+        socket.broadcast.emit('move', msg);
+    });
+    socket.on('play', function (msg) {
+        socket.broadcast.emit('play', msg);
+        console.log("ready " + msg);
+    });
+    socket.on('disconnect', function () {
+        for (let i = 0; i < 100; i++) {
+            if (games[i].pid[0] == playerId || games[i].pid[1] == playerId)
+                games[i].players--;
+        }
+        console.log(playerId + ' disconnected');
     });
 });
--- src/components/MyBoard.vue
+++ src/components/MyBoard.vue
@@ -1,64 +1,59 @@
 <template>
-  <main>
-    <section role="region" aria-label="Board Controls">
-      <button type="button" @click="boardAPI?.board.toggleOrientation()">
-        Toggle orientation
-      </button>
-      <button type="button" @click="boardAPI?.resetBoard()">Reset</button>
-      <button type="button" @click="boardAPI?.undoLastMove()">Undo</button>
-      <button type="button" @click="boardAPI?.toggleThreats()">Threats</button>
-    </section>
-    <TheChessboard :board-config="boardConfig" @board-created="(api) => (boardAPI = api)"
-      @checkmate="handleCheckmate" />
-
-  </main>
+  <div id="myBoard" style="width: 400px"></div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import { TheChessboard } from 'vue3-chessboard';
-import 'vue3-chessboard/style.css';
-// import { io } from "socket.io-client"
-// var socket = io.connect("http://localhost:8888")
-const boardAPI = ref();
-
+import { Chess } from 'chess.js'
+import Chessboard from "chessboardjs-vue3";
 export default {
-  components: {
-    TheChessboard
-  },
-  setup() {
-    function handleCheckmate(isMated) {
-      if (isMated === 'w') {
-        alert('Black wins!');
-      } else {
-        alert('White wins!');
+  data() {
+    return {
+      board: null,
+      game: new Chess(),
+      config: {
+        draggable: true,
+        position: 'start',
+        onDragStart: this.onDragStart,
+        onDrop: this.onDrop,
       }
     }
-
-    function toggleOrientation() {
-      boardAPI.value?.board.toggleOrientation();
+  },
+  methods: {
+    // eslint-disable-next-line
+    onDragStart(source, piece, position, orientation) {
+      if (this.game.isGameOver()) return false
+      if ((this.game.turn() === 'w' && piece.charAt(0) === 'b') ||
+        (this.game.turn() === 'b' && piece.charAt(0) === 'w')) {
+        return false
+      }
+    },
+    onDrop(source, target) {
+      if (source[1] === '1' || source[1] === '8' || target[1] === '1' || target[1] === '8') {
+        var move = this.game.move({
+          from: source,
+          to: target,
+          promotion: 'q' // Try to promote the pawn to a queen
+        });
+      } else {
+        move = this.game.move({
+          from: source,
+          to: target
+        });
+      }
+      console.log(move)
+      if (move) {
+        this.board.position(this.game.fen())
+      } else {
+        return 'snapback'
+      }
+    },
+    onSnapEnd() {
+      this.board.position(this.game.fen())
     }
-
-    function resetBoard() {
-      boardAPI.value?.resetBoard();
-    }
-
-    function handleBoardCreated(api) {
-      boardAPI.value = api;
-      boardAPI.value.registerMoveListener(handleMove);
-    }
-
-    function handleMove(move) {
-      console.log(`Move made: ${move}`);
-    }
-
-    const boardConfig = {
-      coordinates: false,
-      autoCastle: false,
-    };
-
-    return { boardConfig, handleCheckmate, toggleOrientation, resetBoard, handleBoardCreated, handleMove};
+  },
+  mounted() {
+    console.log(this.game.fen())
+    this.board = Chessboard('myBoard', this.config)
   }
-};
-
+}
 </script>
