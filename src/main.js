f648dc4d065d696579164a61f797c11fed10b7ea
UnixUser <wevertonslima@gmail.com>
2022-12-19 19:18:25 -0300

    Instaled bootstrap-vue package

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 const app = createApp(App);
 app.mount('#app');
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
