dcf4cae8a39ebfea57699c5f6f50601fa7ef77f8
UnixUser <38821945+Unix-User@users.noreply.github.com>
2024-07-11 23:57:56 -0300

    fix

--- server/server.js
+++ server/server.js
@@ -4,8 +4,7 @@ require('dotenv').config()
 const serverUrl = process.env.VUE_APP_SERVER_URL + ':' + process.env.VUE_APP_SERVER_PORT
 if (process.env.VUE_APP_MODE == 'prod') {
     console.log('production mode')
-    const port = (process.env.VUE_APP_CLIENT_PORT == '80') ? '' : ":" + process.env.VUE_APP_CLIENT_PORT
-    var clientUrls = process.env.VUE_APP_CLIENT_URLS.split(',').map(url => url + ":" + port);
+    var clientUrls = process.env.VUE_APP_CLIENT_URLS.split(',').map(url => url + ":" + process.env.VUE_APP_CLIENT_PORT);
     var cors = { 
         cors: {
         origin: clientUrls,
